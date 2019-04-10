5c9527c6c06606bacd8f73cb875bc4bb09cfc9af
Alexey Korablin <Alexey_Korablin@epam.com>
2019-04-10 15:40:44 +0300

    004-01_nea Views and layouts

--- meadowlark.js
+++ meadowlark.js
@@ -1,32 +1,32 @@
 const express = require('express');
+const handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
 
 const app = express();
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3053);
 
 app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    res.render('about');
 });
 
 // custom 404 page
-app.use((req, res) => {
-    res.type('text/plain');
+app.use((req, res, next) => {
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 // custom 500 page
 app.use((err, req, res, next) => {
-    console.error()
-    res.type('text/plain');
+    console.error(err.stack);
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'),
@@ -34,8 +34,12 @@ app.listen(app.get('port'),
 
 
 // Help Section
+//
 // .get - to set a route. 
 //      Methods .get, .put, .post etc. get 2 params: a route - string like '/' and function
 // .use - to set a middleware
 // The order of routes and middlewares is significant
 // The handlers for errors 404 and 500 can be destinguished by number of arguments
+// 
+// { defaultLayout: 'main' } - unless you specify otherwise, this is the layout that will be used for any view
+//
